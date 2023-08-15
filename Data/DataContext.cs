c3037a6fc68ad8abad7d99d2851e757751c00e61
seheerst <seherst9@gmail.com>
2023-08-15 19:20:05 +0300

    feat: initialize database

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EticaretWeb.Data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options ): base(options)
+    {
+         
+    }
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; } 
+}
+
