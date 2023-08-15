491124dda584583a858601b8313e611df3e4a061
seheerst <seherst9@gmail.com>
2023-08-15 16:08:44 +0300

    feat: initialize project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using EticaretWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EticaretWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+
+        public IActionResult List()
+        {
+            return View();
+        }
+       
+    }
+}
