a33d22ae5ee319910e5027938de7250a2c38d230
seheerst <seherst9@gmail.com>
2023-08-16 23:35:10 +0300

    feat: database objects added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,28 +1,62 @@
 ﻿using EticaretWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using EticaretWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EticaretWeb.Controllers
 {
     public class HomeController : Controller
     {
-     
+        private readonly DataContext _context;
 
-        public IActionResult Index()
+        public HomeController(DataContext context)
         {
-            return View();
-        }
-        
-        public IActionResult Details()
-        {
-            return View();
+            _context = context;
         }
 
 
-        public IActionResult List()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _context.Products.Where(i => i.IsHome && i.IsApproved).Select(i => new ProductModel()
+            {
+                ProductId = i.ProductId,
+                Name = i.Name.Length > 15 ? i.Name.Substring(0, 47) + "..." : i.Name,
+                Description = i.Description.Length > 50 ? i.Description.Substring(0, 47) + "..." : i.Description,
+                Price = i.Price,
+                Image = i.Image,
+                Stock = i.Stock,
+                CategoryId = i.CategoryId
+            }).ToListAsync();
+            return View(products);
         }
-       
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _context.Products.FindAsync(id));
+        }
+
+
+        public async Task<IActionResult> List(int? id)
+        {
+            var products = _context.Products.Where(i => i.IsApproved).Select(i => new ProductModel()
+            {
+                ProductId = i.ProductId,
+                Name = i.Name.Length > 15 ? i.Name.Substring(0, 47) + "..." : i.Name,
+                Description = i.Description.Length > 50 ? i.Description.Substring(0, 47) + "..." : i.Description,
+                Price = i.Price,
+                Image = i.Image ?? "1.jpg",
+                Stock = i.Stock,
+                CategoryId = i.CategoryId
+            }).AsQueryable();
+
+            if (id != null)
+            {
+                products = products.Where(i => i.CategoryId == id);
+            }
+
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(products.ToList());
+        }
     }
 }
